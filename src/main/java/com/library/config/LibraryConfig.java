3ad83aab0ee90b2cecddcfcb71da84f4f3595465
Mishal010 <arknight265@gmail.com>
2025-08-11 23:24:57 +0530

    Phase 5 begins

---
+++ src/main/java/com/library/config/LibraryConfig.java
@@ -0,0 +1,7 @@
+package com.library.config;
+
+public class LibraryConfig {
+    public static  final int MAX_ISSUED_BOOKS = 3;
+    public static  final int DUE_DAYS = 14;
+    public static  final double FINE_PER_DAY = 10.0;
+}
