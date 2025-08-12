209eca8564cc2660ae5dab2c5fb634cbf5f81083
Mishal010 <arknight265@gmail.com>
2025-08-12 23:59:56 +0530

    Phse 5 stuck withsome concepts

--- src/main/java/com/library/dao/IssuedBooksDAO.java
+++ src/main/java/com/library/dao/IssuedBooksDAO.java
@@ -1,4 +1,10 @@
 package com.library.dao;
 
+import java.sql.Connection;
+
 public class IssuedBooksDAO {
+    private Connection conn;
+    public IssuedBooksDAO(Connection conn){
+        this.conn = conn;
+    }
 }
